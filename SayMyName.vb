a8bf28b075e2ef72537fda98df4b6efc32541a01
cameron coxen <coxecame@isu.edu>
2020-08-30 21:41:34 -0600

    t

--- SayMyName.vb
+++ SayMyName.vb
@@ -2,20 +2,22 @@
 'RCET0265
 'Fall 2020
 'Say My Name
-
+'https://github.com/CalvinAC/SayMyName/blob/master/SayMyName.vb
 
 Module SayMyName
 
     Sub Main()
         'Function used to set the users input as a variable
+
         Dim userNameInput As String
 
         'Text greeting the user and prompting them to input their name
         Console.WriteLine("Hello, and welcome to jurassic park")
         Console.ReadLine()
-        Console.WriteLine("Please enter your name for customized character")
+        Console.WriteLine("Please enter your name")
         Console.ReadLine()
         Console.WriteLine("You entered: " + userNameInput)
+        Console.WriteLine("Enjoy your stay, " + userNameInput)
 
 
         Console.ReadLine()
