0b7ec86ae2f0aa84402ed57eef65b2a33a13cab8
cameron coxen <coxecame@isu.edu>
2020-08-30 21:31:07 -0600

    Add project files.

---
+++ Module1.vb
@@ -0,0 +1,25 @@
+﻿'Calvin Coxen
+'RCET0265
+'Fall 2020
+'Say My Name
+
+
+Module Module1
+
+    Sub Main()
+        'Function used to set the users input as a variable
+        Dim userNameInput As String
+
+        'Text greeting the user and prompting them to input their name
+        Console.WriteLine("Hello, and welcome to jurassic park")
+        Console.ReadLine()
+        Console.WriteLine("Please enter your name for customized character")
+        Console.ReadLine()
+        Console.WriteLine("You entered: " + userNameInput)
+
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
